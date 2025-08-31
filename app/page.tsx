58d0364a420c892698579cd23a1c24a79cea42a5
beau-cloud <beau@opensourcenantucket.com>
2025-08-31 00:49:12 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { Check } from '@/components/Check';
+import { Check } from '../components/Check';
 
 type DiagResponse = {
   ok: boolean;
